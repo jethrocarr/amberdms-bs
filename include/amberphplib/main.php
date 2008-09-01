bf16c3763e412054751fc2688fd5eb1b30119cec
Jethro Carr <jethro.carr@jethrocarr.com>
2008-09-01 15:47:08 +0000

    * Started development of unified AmberPHPLib files

---
+++ include/amberphplib/main.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	AMBERPHPLIB
+
+	PHP functions + classed developed by Amberdms for use in various products.
+
+	All code is Licensed under the GNU GPL version 2. If you wish to use this
+	code in a propietary/commercal product, please contact sales@amberdms.com.
+*/
+
+include("tables.php");
+
+
---
+++ include/amberphplib/tables.php
@@ -0,0 +1,174 @@
+<?php
+/*
+	tables.php
+
+	Provides classes/functions used for generating all the tables and forms
+	used.
+
+	Some of the handy features it provides:
+	* Ability to select/unselect columns to display on tables
+	* Lookups of column names against word database allows for different language translations.
+	* CSV export function
+
+	Class provided is "table"
+
+*/
+
+class table
+{
+	var $columns;			// array containing all columns to be displayed
+	var $columns_order;		// array containing columns to order by
+	
+	var $data;			// table content
+	var $data_num_rows;		// number of rows
+
+	var $sql_table;			// SQL table to get the data from
+	var $sql_query;			// SQL query used
+
+	var $render_columns;		// human readable column names
+
+
+	/*
+		generate_sql()
+
+		This function generates the SQL query to be used for generating the table.
+	*/
+	function generate_sql()
+	{
+		// prepare the select statement
+		$this->sql_query = "SELECT ";
+		
+		foreach ($this->columns as $column)
+		{
+			$this->sql_query .= "$column, ";
+		}
+
+		$this->sql_query .= "id FROM `". $this->sql_table ."` ORDER BY ";
+
+
+		// add the order statements - make sure we don't add an extra comma on the end
+		$count = 0;
+		foreach ($this->columns_order as $column_order)
+		{
+			$count++;
+			
+			if ($count < count($this->columns_order))
+			{
+				$this->sql_query .= $column_order .", ";
+			}
+			else
+			{
+				$this->sql_query .= $column_order;
+			}
+		}
+		
+		$this->sql_query .= " DESC";
+
+		return 1;
+	}
+
+
+	/*
+		generate_data()
+
+		This function executes the SQL statement and fetches all the data from
+		MySQL into an associate array.
+
+		This data can then be used directly to generate the table, or can be
+		modified by other code to produce the desired result before creating
+		the final output.
+
+		Returns the number of rows found.
+	*/
+	function generate_data()
+	{
+		$mysql_result		= mysql_query($this->sql_query);
+		$mysql_num_rows		= mysql_num_rows($mysql_result);
+		$this->data_num_rows	= $mysql_num_rows;
+
+		if (!$mysql_num_rows)
+		{
+			return 0;
+		}
+		else
+		{
+			while ($mysql_data = mysql_fetch_array($mysql_result))
+			{
+				$this->data[] = $mysql_data;
+			}
+
+			return $mysql_num_rows;
+		}
+	}
+
+
+	/*
+		render_column_names($language)
+
+		This function looks up the human-translation of the column names and returns the results.
+
+		Defaults to US english (en_us) if no language is specified.
+	*/
+	function render_column_names($language)
+	{
+		if (!$language)
+			$language = "en_us";
+
+			
+		// create an untranslated array, so that we don't have a complete
+		// failure if a transation is missing.
+		foreach ($this->columns as $label)
+		{
+			$this->render_columns[ $label ] =  $label;
+		}
+
+
+		// prepare the SQL
+		$mysql_string = "SELECT label, translation FROM `language` WHERE language='en_us' AND label=(";
+
+		// list all the column labels
+		$count = 0;
+		foreach ($this->columns as $column)
+		{
+			$count++;
+			
+			if ($count < count($this->columns))
+			{
+				$mysql_string .= "'$column' OR ";
+			}
+			else
+			{
+				$mysql_string .= "'$column'";
+			}
+		}
+
+		$mysql_string .= ")";
+
+		
+		// query
+		$mysql_result	= mysql_query($mysql_string);
+		$mysql_num_rows	= mysql_num_rows($mysql_result);
+
+		if (!$mysql_num_rows)
+		{
+			return 0;
+		}
+		else
+		{
+			while ($mysql_data = mysql_fetch_array($mysql_result))
+			{
+				$this->render_columns[ $mysql_data["label"] ] = $mysql_data["translation"];
+			}
+
+			return 1;
+		}
+	
+	}	
+
+
+
+} // end of table class
+
+
+
+?>
