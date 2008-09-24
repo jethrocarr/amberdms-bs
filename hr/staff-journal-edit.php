189a68ba2652331761b34a5474d97bb5ab7e97d0
Jethro Carr <jethro.carr@jethrocarr.com>
2008-09-24 05:10:12 +0000

    * Implemented employee journal

---
+++ hr/staff-journal-edit.php
@@ -0,0 +1,106 @@
+<?php
+/*
+	staff/journal_edit.php
+	
+	access: staff_write
+
+	Allows the addition or adjustment of journal entries.
+*/
+
+if (user_permissions_get('staff_write'))
+{
+	$id = $_GET["id"];
+
+	// nav bar options.
+	$_SESSION["nav"]["active"]	= 1;
+	
+	$_SESSION["nav"]["title"][]	= "Employee's Details";
+	$_SESSION["nav"]["query"][]	= "page=hr/staff-view.php&id=$id";
+
+	$_SESSION["nav"]["title"][]	= "Employee's Journal";
+	$_SESSION["nav"]["query"][]	= "page=hr/staff-journal.php&id=$id";
+	$_SESSION["nav"]["current"]	= "page=hr/staff-journal.php&id=$id";
+
+
+	function page_render()
+	{
+		$id		= security_script_input('/^[0-9]*$/', $_GET["id"]);
+		$journalid	= security_script_input('/^[0-9]*$/', $_GET["journalid"]);
+		$action		= security_script_input('/^[a-z]*$/', $_GET["action"]);
+		$type		= security_script_input('/^[a-z]*$/', $_GET["type"]);
+
+		
+		/*
+			Journal Forms
+		*/
+
+		$journal_form = New journal_input;
+			
+		// basic details of this entry
+		$journal_form->prepare_set_journalname("staff");
+		$journal_form->prepare_set_journalid($journalid);
+		$journal_form->prepare_set_customid($id);
+
+		// set the processing form
+		$journal_form->prepare_set_form_process_page("hr/staff-journal-edit-process.php");
+
+		
+		if ($action == "delete")
+		{
+			print "<h3>EMPLOYEE JOURNAL - DELETE ENTRY</h3><br>";
+			print "<p>This page allows you to delete an entry from the employee's journal.</p>";
+
+			// render delete form
+			$journal_form->render_delete_form();		
+
+		}
+		else
+		{
+			if ($type == "file")
+			{
+				// file uploader
+				if ($journalid)
+				{
+					print "<h3>EMPLOYEE JOURNAL - UPLOAD FILE</h3><br>";
+					print "<p>This page allows you to attach a file to the employee's journal.</p>";
+				}
+				else
+				{
+					print "<h3>EMPLOYEE JOURNAL - UPLOAD FILE</h3><br>";
+					print "<p>This page allows you to attach a file to the employee's journal.</p>";
+				}
+
+				// edit or add file
+				$journal_form->render_file_form();
+			}
+			else
+			{
+				// default to text
+				if ($journalid)
+				{
+					print "<h3>EMPLOYEE JOURNAL - EDIT ENTRY</h3><br>";
+					print "<p>This page allows you to edit an existing entry in the employee's journal.</p>";
+				}
+				else
+				{
+					print "<h3>EMPLOYEE JOURNAL - ADD ENTRY</h3><br>";
+					print "<p>This page allows you to add an entry to the employee's journal.</p>";
+				}
+
+				// edit or add
+				$journal_form->render_text_form();		
+			}
+			
+		}
+		
+
+
+	} // end page_render
+
+} // end of if logged in
+else
+{
+	error_render_noperms();
+}
+
+?>
---
+++ hr/staff-journal.php
@@ -0,0 +1,85 @@
+<?php
+/*
+	staff/journal.php
+	
+	access: staff_view (read-only)
+		staff_write (write access)
+
+	Standard journal for staff records and audit trail.
+*/
+
+
+if (user_permissions_get('staff_view'))
+{
+	$id = $_GET["id"];
+	
+	// nav bar options.
+	$_SESSION["nav"]["active"]	= 1;
+	
+	$_SESSION["nav"]["title"][]	= "Employee's Details";
+	$_SESSION["nav"]["query"][]	= "page=hr/staff-view.php&id=$id";
+
+	$_SESSION["nav"]["title"][]	= "Employee's Journal";
+	$_SESSION["nav"]["query"][]	= "page=hr/staff-journal.php&id=$id";
+	$_SESSION["nav"]["current"]	= "page=hr/staff-journal.php&id=$id";
+
+
+
+	function page_render()
+	{
+		$id = security_script_input('/^[0-9]*$/', $_GET["id"]);
+
+		/*
+			Title + Summary
+		*/
+		print "<h3>EMPLOYEE'S JOURNAL</h3><br>";
+		print "<p>The journal is a place where you can put your own notes, files and view the history of this employee.</p>";
+
+		print "<p><b><a href=\"index.php?page=hr/staff-journal-edit.php&type=text&id=$id\">Add new journal entry</a> || <a href=\"index.php?page=hr/staff-journal-edit.php&type=file&id=$id\">Upload File</a></b></p>";
+
+
+		// make sure the staff exists
+		$sql = New sql_query;
+		$sql->string = "SELECT id FROM `staff` WHERE id='$id'";
+		$sql->execute();
+		
+		if (!$sql->num_rows())
+		{
+			print "<p><b>Error: The requested employee does not exist. <a href=\"index.php?page=staff/staff.php\">Try looking for your employee on the staff list page.</a></b></p>";
+		}
+		else
+		{
+			/*
+				Define the journal structure
+			*/
+
+			// basic
+			$journal		= New journal_display;
+			$journal->journalname	= "staff";
+			
+			// set the pages to use for forms or file downloads
+			$journal->prepare_set_form_process_page("hr/staff-journal-edit.php");
+			$journal->prepare_set_download_page("hr/staff-journal-download-process.php");
+
+
+			// define SQL structure
+			$journal->sql_obj->prepare_sql_addwhere("customid='$id'");		// we only want journal entries for this ticket!
+
+			// process SQL			
+			$journal->generate_sql();
+			$journal->load_data();
+
+			// display			
+			$journal->render_journal();
+			
+		}
+
+	} // end page_render
+
+} // end of if logged in
+else
+{
+	error_render_noperms();
+}
+
+?>
