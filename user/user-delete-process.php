2862fd796f69a3aad9518bb2ae7eda38bab77fa8
Jethro Carr <jethro.carr@jethrocarr.com>
2008-11-10 22:38:01 +0000

    Implemented user deletion system

--- user/user-delete-process.php
+++ user/user-delete-process.php
@@ -1,65 +1,117 @@
 <?php
-//
-// user/user-delete-process.php
-//
-// delete an administrator user account
-//
+/*
+	user/user-delete-process.php
+
+	access: admin
+
+	Deletes a user account
+*/
 
 // includes
-include_once("../include/database.php");
-include_once("../include/user.php");
-include_once("../include/security.php");
+include_once("../include/config.php");
+include_once("../include/amberphplib/main.php");
 
 
-if (user_permissions_get("admin"))
+if (user_permissions_get('admin'))
 {
-	// check input
-	$userid			= security_form_input("/^[0-9]*$/", "userid", 1, "No user ID supplied.");
-	$confirm		= security_form_input("/^on$/", "confirm", 1, "Please confirm the deletion");
+	/////////////////////////
 
-	if ($_SESSION["error"]["message"])
+	$id				= security_form_input_predefined("int", "id_user", 1, "");
+
+	// these exist to make error handling work right
+	$data["username"]		= security_form_input_predefined("any", "username", 0, "");
+
+	// confirm deletion
+	$data["delete_confirm"]		= security_form_input_predefined("any", "delete_confirm", 1, "You must confirm the deletion");
+
+	
+	// make sure the user actually exists
+	$sql_obj		= New sql_query;
+	$sql_obj->string	= "SELECT id FROM `users` WHERE id='$id'";
+	$sql_obj->execute();
+	
+	if (!$sql_obj->num_rows())
 	{
-		// errors occured
-		$_SESSION["error"]["form"] = "delete";
-		header("Location: ../index.php?page=user/user-details.php&id=$userid");
+		$_SESSION["error"]["message"][] = "The user you have attempted to edit - $id - does not exist in this system.";
+	}
+
+
+		
+	//// ERROR CHECKING ///////////////////////
+			
+
+	/// if there was an error, go back to the entry page
+	if ($_SESSION["error"]["message"])
+	{	
+		$_SESSION["error"]["form"]["user_delete"] = "failed";
+		header("Location: ../index.php?page=user/user-delete.php&id=$id");
 		exit(0);
 	}
 	else
 	{
-		$_SESSION["error"] = array();
 
-		// blow away the user.
-		$mysql_string = "DELETE FROM `users` WHERE id='$userid'";
-		$mysql_result = mysql_query($mysql_string);
-		if (!$mysql_result)
+		/*
+			Delete User
+		*/
+			
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "DELETE FROM users WHERE id='$id'";
+			
+		if (!$sql_obj->execute())
 		{
-			die('MySQL Error: ' . mysql_error());
+			$_SESSION["error"]["message"][] = "A fatal SQL error occured whilst trying to delete the user";
+		}
+		else
+		{		
+			$_SESSION["notification"]["message"][] = "Customer has been successfully deleted.";
 		}
 
-		// delete the user's permissions
-		$mysql_string = "DELETE FROM `users_permissions` WHERE userid='$userid'";
-		$mysql_result = mysql_query($mysql_string);
-		if (!$mysql_result)
+
+		/*
+			Delete user permissions
+			(both access and staff permissions)
+		*/
+		
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "DELETE FROM users_permissions WHERE userid='$id'";
+			
+		if (!$sql_obj->execute())
 		{
-			die('MySQL Error: ' . mysql_error());
+			$_SESSION["error"]["message"][] = "A fatal SQL error occured whilst trying to delete the user permissions";
+		}
+		
+		
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "DELETE FROM users_permissions_staff WHERE userid='$id'";
+			
+		if (!$sql_obj->execute())
+		{
+			$_SESSION["error"]["message"][] = "A fatal SQL error occured whilst trying to delete the user-staff permissions";
 		}
 
-		
-		
-		// goto user admin page
-		$_SESSION["notification"]["message"] = "The administrator has been successfully deleted.";
+
+
+		/*
+			Delete Journal
+		*/
+		journal_delete_entire("users", $id);
+
+
+
+		// return to user list
 		header("Location: ../index.php?page=user/users.php");
 		exit(0);
+	}
 
-	} // if valid data input
-
+	/////////////////////////
 	
-	
-} // end of "is user logged in?"
+}
 else
 {
-	// user does not have permissions to access this page.
+	// user does not have perms to view this page/isn't logged on
 	error_render_noperms();
+	header("Location: ../index.php?page=message.php");
+	exit(0);
 }
 
 
