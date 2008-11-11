49887e2e63df136e93c4bf0928f8e2058e4f4a47
Jethro Carr <jethro.carr@jethrocarr.com>
2008-11-11 03:10:30 +0000

    Wrote function for providing product deletion form

--- include/products/inc_product_forms.php
+++ include/products/inc_product_forms.php
@@ -182,4 +182,113 @@ function products_form_details_render($productid, $mode)
 	
 } // end of products_forms_details_render
 
+
+
+
+/*
+	products_form_delete_render
+
+	Values
+	productid	ID of the product entry
+
+	Return Codes
+	0		failure
+	1		success
+*/
+function products_form_delete_render($productid)
+{
+	log_debug("inc_products_forms", "Executing products_forms_delete_render($productid)");
+
+
+	/*
+		Define form structure
+	*/
+	$form = New form_input;
+	$form->formname = "product_delete";
+	$form->language = $_SESSION["user"]["lang"];
+
+	$form->action = "products/delete-process.php";
+	$form->method = "post";
+
+	// general
+	$structure = NULL;
+	$structure["fieldname"] 	= "code_product";
+	$structure["type"]		= "text";
+	$form->add_input($structure);
+	
+	$structure = NULL;
+	$structure["fieldname"] 	= "name_product";
+	$structure["type"]		= "text";
+	$form->add_input($structure);
+
+
+	// hidden data
+	$structure = NULL;
+	$structure["fieldname"] 	= "id_product";
+	$structure["type"]		= "hidden";
+	$structure["defaultvalue"]	= "$productid";
+	$form->add_input($structure);
+
+
+	// confirm delete
+	$structure = NULL;
+	$structure["fieldname"] 	= "delete_confirm";
+	$structure["type"]		= "checkbox";
+	$structure["options"]["label"]	= "Yes, I wish to delete this product and realise that once deleted the data can not be recovered.";
+	$form->add_input($structure);
+
+
+	// submit button
+	//
+	// We check if the product has been added to any invoices, and then either define
+	// a delete button or a message
+	
+	$structure = NULL;
+	$structure["fieldname"] = "submit";
+	
+	$sql_obj		= New sql_query;
+	$sql_obj->string	= "SELECT id FROM account_items WHERE (type='product' OR type='time') AND customid='$id'";
+	$sql_obj->execute();
+
+	if ($sql_obj->num_rows())
+	{
+		$structure["type"]		= "message";
+		$structure["defaultvalue"]	= "<i>This project can not be deleted because time entries have been assigned to it.</i>";
+	}
+	else
+	{
+		$structure["type"]		= "submit";
+		$structure["defaultvalue"]	= "delete";
+	}
 			
+	$form->add_input($structure);
+
+		
+
+
+	// define subforms
+	$form->subforms["product_delete"]	= array("code_product", "name_product");
+	$form->subforms["hidden"]		= array("id_product");
+	$form->subforms["submit"]		= array("delete_confirm", "submit");
+
+
+	/*
+		Load Data
+	*/
+	$form->sql_query = "SELECT * FROM `products` WHERE id='$productid' LIMIT 1";		
+	$form->load_data();
+
+
+	/*
+		Display Form Information
+	*/
+	$form->render_form();
+
+
+	return 1;
+	
+} // end of products_forms_delete_render
+
+
+
+
