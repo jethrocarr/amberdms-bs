94866bc1bb5f1a4020932b6bff8a4fb4ef93bc3a
Jethro Carr <jethro.carr@jethrocarr.com>
2009-01-04 04:07:10 +0000

    *** empty log message ***

---
+++ include/cron/services.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php
+<?php
+/*
+	include/cron/services.php
+
+	This script is called daily and runs through all the customers and creates new service
+	periods and invoices where required.
+
+	Invoices are then set to customers via email automatically.
+	
+*/
+
+
+// includes
+require("../config.php");
+require("../amberphplib/main.php");
+
+
+// include service logic
+require("../services/inc_services_invoicegen.php");
+
+
+
+print "Checking for service invoices...\n";
+
+
+// generate new service periods
+service_periods_generate(NULL);
+
+// generate any invoices required
+service_invoices_generate(NULL);
+
+
+print "Service invoicing complete.\n";
+
+
+//print_r($_SESSION);
+
+exit(0);
+
