589164aed5a0558b1d6786ba0b3798d1d0bf1c1b
Jethro Carr <jethro.carr@jethrocarr.com>
2009-02-06 03:38:00 +0000

    Updated error messages

--- accounts/taxes/taxes.php
+++ accounts/taxes/taxes.php
@@ -67,11 +67,11 @@ class page_output
 		// display table
 		if (!count($this->obj_table->columns))
 		{
-			print "<p><b>Please select some valid options to display.</b></p>";
+			format_msgbox("important", "<p>Please select some valid options to display.</p>");
 		}
 		elseif (!$this->obj_table->data_num_rows)
 		{
-			print "<p><b>You currently have no taxes in your database.</b></p>";
+			format_msgbox("info", "<p>You currently have no taxes in your database.</p>");
 		}
 		else
 		{
