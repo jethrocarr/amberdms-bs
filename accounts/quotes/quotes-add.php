e8314e7654ba3843752ba0463e257a4b7b940aa0
Jethro Carr <jethro.carr@jethrocarr.com>
2009-02-06 12:23:38 +0000

    Bug Fix: quotes-add.php had not been upgraded to new render framework

--- accounts/quotes/quotes-add.php
+++ accounts/quotes/quotes-add.php
@@ -1,36 +1,57 @@
 <?php
 /*
-	accounts/quotes/quotes-add.php
+	accounts/quotes/quote-add.php
 	
-	access: account_quotes_add
+	access: accounts_quotes_write
 
-	Form to add a new quotes to the database.
+	Provides the form to add a new quotation to the system	
 */
 
 // custom includes
-require("include/accounts/inc_quotes.php");
-require("include/accounts/inc_quotes_details.php");
+require("include/accounts/inc_quotes_forms.php");
 
 
-if (user_permissions_get('accounts_quotes_write'))
+
+class page_output
 {
-	function page_render()
+	var $id;
+	var $obj_menu_nav;
+	var $obj_form_quote;
+
+
+	function check_permissions()
 	{
-		/*
-			Title + Summary
-		*/
-		print "<h3>ADD QUOTE</h3><br>";
+		return user_permissions_get("accounts_quotes_write");
+	}
+
+
+	function check_requirements()
+	{
+		// nothing to check
+		return 1;
+	}
+
+
+	function execute()
+	{
+		$this->obj_form_quote			= New quote_form_details;
+		$this->obj_form_quote->type		= "ar";
+		$this->obj_form_quote->quoteid		= 0;
+		$this->obj_form_quote->processpage	= "accounts/quotes/quotes-edit-process.php";
+		
+		$this->obj_form_quote->execute();
+	}
+
+	function render_html()
+	{
+		// heading
+		print "<h3>CREATE QUOTATION</h3><br>";
 		print "<p>This page provides features to allow you to add new quotes to the system.</p>";
 
-		quotes_form_details_render(0, "accounts/quotes/quotes-edit-process.php");
-
-
-	} // end page_render
-
-} // end of if logged in
-else
-{
-	error_render_noperms();
+		// display form
+		$this->obj_form_quote->render_html();
+	}
+	
 }
 
 ?>
