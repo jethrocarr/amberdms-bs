f0f9767cd4ea8307bb8238cc3904e173a38d102d
Jethro Carr <jethro.carr@jethrocarr.com>
2009-02-07 15:09:59 +0000

    Bug Fix: Enforce SSL connections (security issue)

--- index.php
+++ index.php
@@ -19,6 +19,18 @@ log_debug("index", "Starting index.php");
 
 
 /*
+	Enforce HTTPS
+*/
+if (!$_SERVER["HTTPS"])
+{
+	header("Location: https://". $_SERVER["SERVER_NAME"] ."/".  $_SERVER['PHP_SELF'] );
+	exit(0);
+}
+
+
+
+
+/*
 	Fetch the page name to display, and perform security checks
 */
 
