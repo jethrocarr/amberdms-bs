5cf9fcb37bb4e4988543028387199e21c60bb095
Jethro Carr <jethro.carr@jethrocarr.com>
2009-02-10 04:07:01 +0000

    Added links to overview page

--- accounts/reports/reports.php
+++ accounts/reports/reports.php
@@ -26,8 +26,29 @@ class page_output
 
 	function render_html()
 	{
-		print "<h3>Reports</h3>";
-		print "<p>Have links and general details about the different reports here</p>";
+		print "<h3>ACCOUNTING REPORTS</h3>";
+		print "<p>Select one of the report types below to configure and view financial reports.</p>";
+
+
+		// trial balance
+		format_linkbox("default", "index.php?page=accounts/reports/trialbalance.php", "<p><b>TRIAL BALANCE</b></p>
+			<p>Lists all the accounts which transactions are filed against and provides a basic
+			overview of the current state of the accounts.</p>");
+
+		// income statement
+		print "<br>";
+		format_linkbox("default", "index.php?page=accounts/reports/incomestatement.php", "<p><b>INCOME STATEMENT</b></p>
+			<p>Reports income and expenses for a selected time period.</p>");
+
+		
+		// balance sheet
+		print "<br>";
+		format_linkbox("default", "index.php?page=accounts/reports/balancesheet.php", "<p><b>BALANCE SHEET</b></p>
+			<p>Shows assets, liabilities and equity for the selected time period.</p>");
+
+
+
+			
 	}
 }
 
