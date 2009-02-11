b03b44a2ff4072f9957374da2a857871898cf300
Jethro Carr <jethro.carr@jethrocarr.com>
2009-02-11 02:58:30 +0000

    Wrote main overview page

--- home.php
+++ home.php
@@ -39,23 +39,99 @@ else
 
 		function render_html()
 		{
-			print "<h3>SYSTEM OVERVIEW</h3>";
-			print "<p>Overview of all features needs to go here</p>";
+			print "<h3>OVERVIEW</h3>";
+			print "<p>Welcome to the Amberdms Billing System.</p>";
+
+			print "<p>To get started, use the links above or download the user guide for step-by-step instructions for
+				using this program.</p>";
+
+			print "<a target=\"new\" href=\"help/user_guide.pdf\"><img src=\"images/buttons/button_user_guide.png\" border=\"0\"></img></a><br>";
+
+			print "<br><br>";
 
 
-			// TODO: Complete this page
 
 			/*
-				Unprocessed Invoices
-			*/
-			if (user_permissions_get("accounts_ar_view"))
-			{
-				print "<table width=\"100%\"><tr><td>";
+				COMMERCIAL SUPPORT PACKAGES
 
-//				$sql_obj		= New sql_query;
+				This section links to the amberdms website based on the user's subscription
+				status to make it easy to get commercial support.
+			*/
+			
+			print "<br><br>";
+			print "<h3>COMMERCIAL SUPPORT</h3>";
+
+			// get the support setting from the DB to display the suitable support package information
+			// to the end user.
+			$support_package	= sql_get_singlevalue("SELECT value FROM config WHERE name='SUBSCRIPTION_SUPPORT' LIMIT 1");
+			$subscription_id	= sql_get_singlevalue("SELECT value FROM config WHERE name='SUBSCRIPTION_ID' LIMIT 1");
+
+
+			switch ($support_package)
+			{
+				case "package":
+
+					print "<p>For technical support or user assistance, please use the button below to view
+						how much support you have and support contact details.</p>";
+
+					print "<a target=\"new\" href=\"http://www.amberdms.com/products/billing_system/getsupport.php?subscription=$subscription_id\"><img src=\"images/buttons/button_commercial_support.png\" border=\"0\"></img></a><br>";
+				break;
+
+
+				case "basic":
 				
-				print "</td></tr></table>";
+					print "<p>Amberdms provides unlimited support for any technical issues with your software
+						subscription for no change, however you are also eligable for phone or email
+						based user assistance.</p>";
+
+					print "<p>There is no change for technical support, but user assistance is charged to your
+						account and billed monthly - pricing information is available at the link below.</p>";
+
+					print "<a target=\"new\" href=\"http://www.amberdms.com/products/billing_system/getsupport.php?subscription=$subscription_id\"><img src=\"images/buttons/button_commercial_support.png\" border=\"0\"></img></a><br>";
+
+				break;
+
+
+
+				case "none":
+				default:
+					
+					print "<p>If you would commercial support for the Amberdms Billing System, please click the
+						button below to view our technical & user support packages.</p>";
+
+					print "<p>Amberdms also provided hosted versions of the Amberdms Billing System, eliminating
+						the expense and hassles of running your own servers and can include different support
+						packages with this service.</p>";
+
+					print "<a target=\"new\" href=\"http://www.amberdms.com/products/billing_system/support.php\"><img src=\"images/buttons/button_commercial_support.png\" border=\"0\"></img></a><br>";
+				break;
 			}
+
+			/*
+				Customisations
+			*/
+			print "<br><br>";
+			print "<h3>CUSTOMISATIONS</h3>";
+
+			print "<p>Not everyone has the exact same business, which is why Amberdms offers product customisation services. If you
+				require a particular feature or want to integrate other products with the Amberdms Billing System, talk to us
+				and we can give you competitive quotes and service from the developers who designed and wrote this program.</p>";
+
+			print "<a target=\"new\" href=\"http://www.amberdms.com/products/billing_system/customisations.php\"><img src=\"images/buttons/button_customisations.png\" border=\"0\"></img></a><br>";
+
+
+
+			/*
+				Open Source
+			*/
+			print "<br><br>";
+			print "<h3>OPEN SOURCE</h3>";
+
+			print "<p>The Amberdms Billing System is a fully open source program under
+				the GNU AGPL software license - this means anyone can download the source
+				code for this program and make modifications or run it on your own servers.</p>";
+
+			print "<a target=\"new\" href=\"http://www.amberdms.com/products/billing_system/source.php\"><img src=\"images/buttons/button_getthesource.png\" border=\"0\"></img></a><br>";
 		}
 	}
 }
