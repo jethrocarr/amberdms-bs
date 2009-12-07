4d41fd8b9f209831a17dab4ec5f8c6d017bc82fb
Jethro Carr <jethro.carr@jethrocarr.com>
2009-12-07 12:19:14 +0000

    * Updated product documentation for 1.4.0 release

--- help/help.php
+++ help/help.php
@@ -41,11 +41,31 @@ class page_output
 
 
 		// administration guides
-		print "<br><br>";
-		print "<h3>SYSADMIN MANUALS</h3>";
-		print "<p>The following manual are intended for system administrators installing or upgrading this program.</p>";
-		print "<a class=\"button\" target=\"new\" href=\"help/manual/amberdms_billing_system_installguide.pdf\">Download Installation Guide</a><br>";
-		print "<br>";
+		// (these options differ depending on where you are running the application)
+		$subscription_support	= sql_get_singlevalue("SELECT value FROM config WHERE name='SUBSCRIPTION_SUPPORT' LIMIT 1");
+
+		switch ($subscription_support)
+		{
+			case "hosted":
+			case "hosted_phone":
+				print "<br><br>";
+				print "<h3>SYSADMIN MANUALS</h3>";
+				print "<p>You are running the hosted version of the Amberdms Billing System, so no installation or upgrades are required. However, if you are interested in deploying the Amberdms Billing System open source releases elsewhere, you can download the installation manuals from our website.</p>";
+				print "<a target=\"new\" class=\"button\" href=\"http://www.amberdms.com/?cms=opensource_billing\">Installation Instructions</a><br>";
+				print "<br>";
+			break;
+
+			case "none":
+			case "opensource":
+			default:
+				print "<br><br>";
+				print "<h3>SYSADMIN MANUALS</h3>";
+				print "<p>The following manuals are intended for system administrators installing or upgrading this program.</p>";
+				print "<a class=\"button\" target=\"new\" href=\"help/manual/amberdms_billing_system_install_supported.pdf\">Installation on Supported Platforms</a><br><br>";
+				print "<a class=\"button\" target=\"new\" href=\"help/manual/amberdms_billing_system_install_manual.pdf\">Manual Installation for Unsupported Platforms</a><br>";
+				print "<br>";
+			break;
+		}
 
 
 		// developer manual
