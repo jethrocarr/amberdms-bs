b05691b7b9a61ec37cd7b40e8bcab1f3d9af8024
Jethro Carr <jethro.carr@jethrocarr.com>
2010-03-27 04:45:26 +0000

    * Added new UI pages for setting CDR overrides

---
+++ services/cdr-override-edit.php
@@ -0,0 +1,264 @@
+<?php
+/*
+	services/cdr-override-edit.php
+
+	access: services_write
+
+	Form to add or edit CDR rate overrides.
+*/
+
+
+require("include/services/inc_services.php");
+require("include/services/inc_services_cdr.php");
+
+
+class page_output
+{
+	var $id;			// service ID
+	var $service_type;		// service type (string)
+
+	var $obj_cdr_rate_table;	// CDR rates information
+	var $obj_service;		// service information
+
+	var $obj_table;
+	var $obj_menu_nav;
+
+
+	function page_output()
+	{
+		// init
+		$this->obj_cdr_rate_table	= New cdr_rate_table_rates;
+		$this->obj_service		= New service;
+
+
+		// fetch key service details
+		$this->obj_service->id			= @security_script_input('/^[0-9]*$/', $_GET["id"]);
+		$this->obj_cdr_rate_table->id_rate	= @security_script_input('/^[0-9]*$/', $_GET["id_rate"]);
+
+		$this->service_type			= sql_get_singlevalue("SELECT service_types.name as value FROM services LEFT JOIN service_types ON service_types.id = services.typeid WHERE services.id='". $this->obj_service->id ."' LIMIT 1");
+
+
+		// define the navigiation menu
+		$this->obj_menu_nav = New menu_nav;
+
+		$this->obj_menu_nav->add_item("Service Details", "page=services/view.php&id=". $this->obj_service->id ."");
+		$this->obj_menu_nav->add_item("Service Plan", "page=services/plan.php&id=". $this->obj_service->id ."");
+
+		if ($this->service_type == "bundle")
+		{
+			$this->obj_menu_nav->add_item("Bundle Components", "page=services/bundles.php&id=". $this->obj_service->id ."");
+		}
+
+		if ($this->service_type == "phone_services")
+		{
+			$this->obj_menu_nav->add_item("Call Rate Override", "page=services/cdr-override.php&id=". $this->obj_service->id ."", TRUE);
+		}
+
+		$this->obj_menu_nav->add_item("Service Journal", "page=services/journal.php&id=". $this->obj_service->id ."");
+
+		if (user_permissions_get("services_write"))
+		{
+			$this->obj_menu_nav->add_item("Delete Service", "page=services/delete.php&id=". $this->obj_service->id ."");
+		}
+	}
+
+
+	function check_permissions()
+	{
+		return user_permissions_get("services_write");
+	}
+
+
+	function check_requirements()
+	{
+		// verify that the service exists
+		if ($this->obj_service->verify_id())
+		{
+			$this->obj_service->load_data();
+
+			// verify the rate is valid
+			if ($this->obj_service->data["id_rate_table"])
+			{
+				$this->obj_cdr_rate_table->id	= $this->obj_service->data["id_rate_table"];
+
+				if (!$this->obj_cdr_rate_table->verify_id())
+				{
+					log_write("error", "page_output", "The requested CDR rate table is invalid, there may be some problems with the information in the database.");
+					return 0;
+				}
+			}
+			else
+			{
+				log_write("error", "page_output", "You have yet to set a CDR Rate Table for this service to use - please do so using the plan page before attempting to override the rates");
+				return 0;
+			}
+		}
+		else
+		{
+			log_write("error", "page_output", "The requested service (". $this->obj_service->id .") does not exist - possibly the service has been deleted.");
+			return 0;
+		}
+
+		unset($sql_obj);
+
+
+		// verify that this is a phone_services service
+		if ($this->service_type != "phone_services")
+		{
+			log_write("error", "page_output", "The requested service is not a phone service.");
+			return 0;
+		}
+		
+
+
+		return 1;
+	}
+
+
+	function execute()
+	{
+		// define basic form details
+		$this->obj_form = New form_input;
+		$this->obj_form->formname = "cdr_rate_table_item_edit";
+		$this->obj_form->language = $_SESSION["user"]["lang"];
+
+		$this->obj_form->action = "services/cdr-rates-items-edit-process.php";
+		$this->obj_form->method = "post";
+
+
+
+		// service details
+		$structure = NULL;
+		$structure["fieldname"] 	= "service_name";
+		$structure["type"]		= "text";
+		$this->obj_form->add_input($structure);
+		
+		$structure = NULL;
+		$structure["fieldname"]		= "service_description";
+		$structure["type"]		= "text";
+		$this->obj_form->add_input($structure);
+
+
+		// rate table details
+		$structure = NULL;
+		$structure["fieldname"] 	= "rate_table_name";
+		$structure["type"]		= "text";
+		$this->obj_form->add_input($structure);
+		
+		$structure = NULL;
+		$structure["fieldname"]		= "rate_table_description";
+		$structure["type"]		= "text";
+		$this->obj_form->add_input($structure);
+
+
+		// item options
+		$structure = NULL;
+		$structure["fieldname"]		= "rate_prefix";
+		$structure["type"]		= "input";
+		$structure["options"]["req"]	= "yes";
+		$this->obj_form->add_input($structure);
+
+		$structure = NULL;
+		$structure["fieldname"]		= "rate_description";
+		$structure["type"]		= "input";
+		$structure["options"]["req"]	= "yes";
+		$this->obj_form->add_input($structure);
+
+		$structure = NULL;
+		$structure["fieldname"]		= "rate_price_sale";
+		$structure["type"]		= "input";
+		$structure["options"]["req"]	= "yes";
+		$this->obj_form->add_input($structure);
+
+		$structure = NULL;
+		$structure["fieldname"]		= "rate_price_cost";
+		$structure["type"]		= "input";
+		$structure["options"]["req"]	= "yes";
+		$this->obj_form->add_input($structure);
+
+
+
+		// hidden
+		$structure = NULL;
+		$structure["fieldname"] 	= "id_service";
+		$structure["type"]		= "hidden";
+		$structure["defaultvalue"]	= $this->obj_service->id;
+		$this->obj_form->add_input($structure);
+
+		$structure = NULL;
+		$structure["fieldname"] 	= "id_rate";
+		$structure["type"]		= "hidden";
+		$structure["defaultvalue"]	= $this->obj_cdr_rate_table->id_rate;
+		$this->obj_form->add_input($structure);
+
+		// submit button
+		$structure = NULL;
+		$structure["fieldname"] 	= "submit";
+		$structure["type"]		= "submit";
+		$structure["defaultvalue"]	= "submit";
+		$this->obj_form->add_input($structure);
+		
+
+		// define subforms
+		$this->obj_form->subforms["service_details"]	= array("service_name", "service_description");
+		$this->obj_form->subforms["rate_table_details"]	= array("rate_table_name", "rate_table_description");
+		$this->obj_form->subforms["rate_table_items"]	= array("rate_prefix", "rate_description", "rate_price_sale", "rate_price_cost");
+		$this->obj_form->subforms["hidden"]		= array("id_service", "id_rate");
+		$this->obj_form->subforms["submit"]		= array("submit");
+		
+
+		// load any data returned due to errors
+		if (error_check())
+		{
+			$this->obj_form->load_data_error();
+		}
+		else
+		{
+			$this->obj_cdr_rate_table->load_data();
+			$this->obj_cdr_rate_table->load_data_rate();
+
+			$this->obj_cdr_rate_table->option_type		= "service";
+			$this->obj_cdr_rate_table->option_id		= $this->obj_service->id;
+
+			$this->obj_cdr_rate_table->load_data_rate_override();
+
+
+			$this->obj_form->structure["service_name"]["defaultvalue"]		= $this->obj_service->data["name_service"];
+			$this->obj_form->structure["service_description"]["defaultvalue"]	= $this->obj_service->data["description"];
+
+			$this->obj_form->structure["rate_table_name"]["defaultvalue"]		= $this->obj_cdr_rate_table->data["rate_table_name"];
+			$this->obj_form->structure["rate_table_description"]["defaultvalue"]	= $this->obj_cdr_rate_table->data["rate_table_description"];
+
+			$this->obj_form->structure["rate_prefix"]["defaultvalue"]		= $this->obj_cdr_rate_table->data_rate["rate_prefix"];
+			$this->obj_form->structure["rate_description"]["defaultvalue"]		= $this->obj_cdr_rate_table->data_rate["rate_description"];
+			$this->obj_form->structure["rate_price_sale"]["defaultvalue"]		= $this->obj_cdr_rate_table->data_rate["rate_price_sale"];
+			$this->obj_form->structure["rate_price_cost"]["defaultvalue"]		= $this->obj_cdr_rate_table->data_rate["rate_price_cost"];
+		}
+	}
+
+
+
+	function render_html()
+	{
+		// title and summary
+		if ($this->obj_cdr_rate_table->id_rate)
+		{
+			print "<h3>OVERRIDE CDR RATE</h3><br>";
+			print "<p>Use the form below to set a CDR override that will take preference compared to the standard rate in the table.</p>";
+		}
+		else
+		{
+			print "<h3>ADD OVERRIDE PREFIX</h3><br>";
+			print "<p>Use the form below to add an override prefix.</p>";
+		}
+
+		// display the form
+		$this->obj_form->render_form();
+	}
+
+
+
+}
+
+
+?>
---
+++ services/cdr-override.php
@@ -0,0 +1,224 @@
+<?php
+/*
+	services/cdr-override.php
+
+	access: services_view
+		services_write
+
+	Shows the rates for the selected CDR call and allows them to be overridden.
+*/
+
+
+require("include/services/inc_services.php");
+require("include/services/inc_services_cdr.php");
+
+
+class page_output
+{
+	var $id;		// service ID
+	var $service_type;	// service type (string)
+
+	var $obj_cdr_rate_table;
+	var $obj_table;
+	var $obj_menu_nav;
+
+
+	function page_output()
+	{
+		// init
+		$this->obj_cdr_rate_table	= New cdr_rate_table_rates_override;
+
+
+		// fetch key service details
+		$this->id		= @security_script_input('/^[0-9]*$/', $_GET["id"]);
+		$this->service_type	= sql_get_singlevalue("SELECT service_types.name as value FROM services LEFT JOIN service_types ON service_types.id = services.typeid WHERE services.id='". $this->id ."' LIMIT 1");
+
+		$this->obj_cdr_rate_table->option_type		= "service";
+		$this->obj_cdr_rate_table->option_type_id	= $this->id;
+
+
+		// define the navigiation menu
+		$this->obj_menu_nav = New menu_nav;
+
+		$this->obj_menu_nav->add_item("Service Details", "page=services/view.php&id=". $this->id ."");
+		$this->obj_menu_nav->add_item("Service Plan", "page=services/plan.php&id=". $this->id ."");
+
+		if ($this->service_type == "bundle")
+		{
+			$this->obj_menu_nav->add_item("Bundle Components", "page=services/bundles.php&id=". $this->id ."");
+		}
+
+		if ($this->service_type == "phone_services")
+		{
+			$this->obj_menu_nav->add_item("Call Rate Override", "page=services/cdr-override.php&id=". $this->id ."", TRUE);
+		}
+
+		$this->obj_menu_nav->add_item("Service Journal", "page=services/journal.php&id=". $this->id ."");
+
+		if (user_permissions_get("services_write"))
+		{
+			$this->obj_menu_nav->add_item("Delete Service", "page=services/delete.php&id=". $this->id ."");
+		}
+	}
+
+
+	function check_permissions()
+	{
+		return user_permissions_get("services_view");
+	}
+
+
+	function check_requirements()
+	{
+		// verify that the service exists
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "SELECT id, id_rate_table FROM services WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+		if ($sql_obj->num_rows())
+		{
+			$sql_obj->fetch_array();
+			
+			// verify the rate is valid
+			if ($sql_obj->data[0]["id_rate_table"])
+			{
+				$this->obj_cdr_rate_table->id	= $sql_obj->data[0]["id_rate_table"];
+
+				if (!$this->obj_cdr_rate_table->verify_id())
+				{
+					log_write("error", "page_output", "The requested CDR rate table is invalid, there may be some problems with the information in the database.");
+					return 0;
+				}
+			}
+			else
+			{
+				log_write("error", "page_output", "You have yet to set a CDR Rate Table for this service to use - please do so using the plan page before attempting to override the rates");
+				return 0;
+			}
+		}
+		else
+		{
+			log_write("error", "page_output", "The requested service (". $this->id .") does not exist - possibly the service has been deleted.");
+			return 0;
+		}
+
+		unset($sql_obj);
+
+
+		// verify that this is a phone_services service
+		if ($this->service_type != "phone_services")
+		{
+			log_write("error", "page_output", "The requested service is not a phone service.");
+			return 0;
+		}
+		
+
+
+		return 1;
+	}
+
+
+	function execute()
+	{
+		/*
+			Load CDR Data
+		*/
+
+		// get the rate table info for display purposes
+		$this->obj_cdr_rate_table->load_data();
+
+		// get the values
+		$this->obj_cdr_rate_table->load_data_rate_all();
+		$this->obj_cdr_rate_table->load_data_rate_all_override();
+
+		// get all the overrides in use
+
+
+
+		/*
+			Draw Display Table
+		*/
+
+
+		// establish a new table object
+		$this->obj_table = New table;
+
+		$this->obj_table->language	= $_SESSION["user"]["lang"];
+		$this->obj_table->tablename	= "service_cdr_override";
+
+		// define all the columns and structure
+		$this->obj_table->add_column("standard", "rate_prefix", "");
+		$this->obj_table->add_column("standard", "rate_description", "");
+		$this->obj_table->add_column("money", "rate_price_sale", "");
+		$this->obj_table->add_column("money", "rate_price_cost", "");
+		$this->obj_table->add_column("standard", "rate_override", "");
+
+
+		// defaults
+		$this->obj_table->columns		= array("rate_prefix", "rate_description", "rate_price_sale", "rate_price_cost", "rate_override");
+
+		// custom-load the service rate data
+		$this->obj_table->data_num_rows		= count(array_keys($this->obj_cdr_rate_table->data["rates"]));
+
+
+		$i = 0;
+		foreach (array_keys($this->obj_cdr_rate_table->data["rates"]) as $rate_prefix)
+		{
+			$this->obj_table->data[$i]["id_rate"]		= $this->obj_cdr_rate_table->data["rates"][ $rate_prefix ]["id_rate"];
+			$this->obj_table->data[$i]["id_rate_override"]	= $this->obj_cdr_rate_table->data["rates"][ $rate_prefix ]["id_rate_override"];
+
+			$this->obj_table->data[$i]["rate_prefix"]	= $this->obj_cdr_rate_table->data["rates"][ $rate_prefix ]["rate_prefix"];
+			$this->obj_table->data[$i]["rate_description"]	= $this->obj_cdr_rate_table->data["rates"][ $rate_prefix ]["rate_description"];
+			$this->obj_table->data[$i]["rate_price_sale"]	= $this->obj_cdr_rate_table->data["rates"][ $rate_prefix ]["rate_price_sale"];
+			$this->obj_table->data[$i]["rate_price_cost"]	= $this->obj_cdr_rate_table->data["rates"][ $rate_prefix ]["rate_price_cost"];
+
+			if ($this->obj_cdr_rate_table->data["rates"][ $rate_prefix ]["id_rate_override"])
+			{
+				$this->obj_table->data[$i]["rate_override"] = "<font class=\"table_highlight_important\">SERVICE OVERRIDE</font>";
+			}
+
+			$i++;
+		}
+	}
+
+
+	function render_html()
+	{
+		// Title + Summary
+		print "<h3>CDR RATE OVERRIDE</h3><br>";
+		print "<p>This page allows you to view the rates in the selected rate table for this service, as well as allowing them to be overridden to offer service-specific pricing variations.</p>";
+
+
+		if (user_permissions_get("services_write"))
+		{
+			// details link
+			$structure = NULL;
+			$structure["logic_if"]["column"]	= "id_rate_override";
+			$structure["id"]["value"]		= $this->id;
+			$structure["prefix"]["column"]		= "rate_prefix";
+			$this->obj_table->add_link("tbl_lnk_details", "services/cdr-override-edit.php", $structure);
+
+			// delete link
+			$structure = NULL;
+			$structure["logic_if"]["column"]	= "id_rate_override";
+			$structure["id"]["value"]		= $this->id;
+			$structure["prefix"]["column"]		= "rate_prefix";
+			$structure["full_link"]			= "yes";
+			$this->obj_table->add_link("tbl_lnk_delete", "services/cdr-override-delete-process.php", $structure);
+		}
+
+		// display the table
+		$this->obj_table->render_table_html();
+
+
+		// add link
+		if (user_permissions_get("services_write"))
+		{
+			print "<p><a class=\"button\" href=\"index.php?page=services/cdr-override-edit.php&id=". $this->id ."\">Add New Prefix Rate</a></p>";
+		}
+
+	}	
+
+}
+
+?>
