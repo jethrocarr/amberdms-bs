7a6f4d4ed7625f2b6014f7469d19f279a8d04444
Jethro Carr <jethro.carr@jethrocarr.com>
2010-04-11 23:20:32 +0000

    * DDI changes

--- include/services/inc_services_cdr.php
+++ include/services/inc_services_cdr.php
@@ -1208,4 +1208,336 @@ class cdr_usage
 
 
 
+
+/*
+	CLASS: cdr_customer_service_ddi
+
+	Functions for managing DDI values for customers as well as being able to query
+	a specific DDI to discover what customer it belongs to.
+*/
+
+class cdr_customer_service_ddi
+{
+	var $id;			// ID of the DDI record
+	var $data;			// DDI record data/values to change.
+
+	var $id_customer;		//
+	var $id_service_customer;	//
+
+
+
+	/*
+		verify_id
+
+		Verify that the supplied ID is valid and fetch the customer and service-customer IDs that go along with it.
+
+		Results
+		0	Failure to find the ID
+		1	Success
+	*/
+
+	function verify_id()
+	{
+		log_debug("cdr_customer_services_ddi", "Executing verify_id_service_ddi()");
+
+		if ($this->id)
+		{
+			$sql_obj		= New sql_query;
+			$sql_obj->string	= "SELECT id_service_customer, services_customers.customerid as id_customer FROM `services_customers_ddi` LEFT JOIN services_customers ON services_customers.id = services_customers_ddi.id_service_customer WHERE services_customers_ddi.id='". $this->id ."' LIMIT 1";
+			$sql_obj->execute();
+
+			if ($sql_obj->num_rows())
+			{
+				$sql_obj->fetch_array();
+
+
+				// verify id_service_customer
+				if ($this->id_service_customer)
+				{
+					if ($sql_obj->data[0]["id_service_customer"] == $this->id_service_customer)
+					{
+						log_write("debug", "customers_services_ddi", "The selected service-customer matches the DDI");
+					}
+					else
+					{
+						log_write("error", "customers_services_ddi", "The seleced service-customer (". $this->id_service_customer .") does not match the selected customer (". $this->id .").");
+						return 0;
+					}
+				}
+				else
+				{
+					$this->id_service_customer = $sql_obj->data[0]["id_service_customer"];
+
+					log_write("debug", "customers_services_ddi", "Setting id_service_customer to ". $this->id_service_customer ."");
+				}
+
+
+				// verify customer ID
+				if ($this->id_customer)
+				{
+					if ($sql_obj->data[0]["id_customer"] == $this->id_customer)
+					{
+						log_write("debug", "customers_services_ddi", "The selected DDI belongs to the correct customer and service-customer mapping");
+						return 1;
+					}
+					else
+					{
+						log_write("error", "customers_services_ddi", "The seelcted DDI does not belong to the selected customer ". $this->id ."");
+						return 0;
+					}
+
+				}
+				else
+				{
+					$this->id_customer = $sql_obj->data[0]["id_customer"];
+
+					log_write("error", "Setting id_customer to ". $this->id ."");
+					return 1;
+				}
+
+			}
+		}
+
+		return 0;
+
+	} // end of verify_id_service_ddi
+
+
+
+	/*
+		verify_unique_ddi
+
+		Verifies that the supplied DDI information is not already used by any other DDI
+
+		Results
+		0	Failure - ddi is assigned to another customer
+		1	Success - ddi is available
+	*/
+
+	function verify_unique_ddi()
+	{
+		log_debug("cdr_customer_services_ddi", "Executing verify_unique_ddi()");
+/*
+		TODO: write me
+
+		$sql_obj			= New sql_query;
+		$sql_obj->string		= "SELECT id FROM `services_customers_ddi` WHERE r='". $this->data["code_customer"] ."' ";
+
+		if ($this->id)
+			$sql_obj->string	.= " AND id!='". $this->id ."'";
+
+		$sql_obj->string		.= " LIMIT 1";
+		$sql_obj->execute();
+
+		if ($sql_obj->num_rows())
+		{
+			return 0;
+		}
+*/
+		return 1;
+
+	} // end of verify_unique_ddi
+
+
+
+	/*
+		load_data
+
+		Load the DDI data
+
+		Results
+		0	Failure
+		1	Success
+	*/
+	function load_data()
+	{
+		log_write("debug", "cdr_customer_services_ddi", "Executing load_data_ddi()");
+
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "SELECT ddi_start, ddi_finish, description FROM services_customers_ddi WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+		if ($sql_obj->num_rows())
+		{
+			$sql_obj->fetch_array();
+
+			$this->data = $sql_obj->data[0];
+
+			return 1;
+		}
+
+		return 0;
+
+	} // end of load_data_ddi
+	
+
+
+	/*
+		action_create
+
+		Create a new DDI record based on the data in $this->data
+
+		Results
+		0	Failure
+		#	Success - return ID
+	*/
+	function action_create()
+	{
+		log_debug("cdr_customer_services_ddi", "Executing action_create()");
+
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "INSERT INTO `services_customers_ddi` (id_service_customer) VALUES ('". $this->id_service_customer . "')";
+		$sql_obj->execute();
+
+		$this->id = $sql_obj->fetch_insert_id();
+
+		return $this->id;
+
+	} // end of action_create
+
+
+
+
+	/*
+		action_update
+
+		Updates DDI action
+
+		Returns
+		0	failure
+		#	success - returns the ID
+	*/
+	function action_update()
+	{
+		log_debug("cdr_customer_services_ddi", "Executing action_update()");
+
+		/*
+			Start Transaction
+		*/
+		$sql_obj = New sql_query;
+		$sql_obj->trans_begin();
+
+
+
+		/*
+			If no ID supplied, create a new DDI first
+		*/
+		if (!$this->id)
+		{
+			$mode = "create";
+
+			if (!$this->action_create())
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			$mode = "update";
+		}
+
+
+
+		/*
+			Update DDI value
+		*/
+
+		$sql_obj->string	= "UPDATE `services_customers_ddi` SET "
+						."ddi_start='". $this->data["ddi_start"] ."', "
+						."ddi_finish='". $this->data["ddi_finish"] ."', "
+						."description='". $this->data["description"] ."' "
+						."WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+		
+		/*
+			Commit
+		*/
+
+		if (error_check())
+		{
+			$sql_obj->trans_rollback();
+
+			log_write("error", "cdr_customers_services_ddi", "An error occured when updating customer DDI records.");
+
+			return 0;
+		}
+		else
+		{
+			$sql_obj->trans_commit();
+
+			if ($mode == "update")
+			{
+				log_write("notification", "cdr_customers_services_ddi", "Customer DDI records successfully updated.");
+			}
+			else
+			{
+				log_write("notification", "cdr_customers_service_ddi", "Customer DDI records successfully created.");
+			}
+			
+			return $this->id;
+		}
+
+	} // end of action_update
+
+
+
+	/*
+		action_delete
+
+		Deletes a DDI
+
+		Results
+		0	failure
+		1	success
+	*/
+	function action_delete()
+	{
+		log_debug("cdr_customers_services_ddi", "Executing action_delete()");
+
+
+		/*
+			Start Transaction
+		*/
+
+		$sql_obj = New sql_query;
+		$sql_obj->trans_begin();
+
+
+		/*
+			Delete DDI
+		*/
+			
+		$sql_obj->string	= "DELETE FROM services_customers_ddi WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+
+		/*
+			Commit
+		*/
+		
+		if (error_check())
+		{
+			$sql_obj->trans_rollback();
+
+			log_write("error", "cdr_customers_services_ddi", "An error occured whilst trying to delete the DDI.");
+
+			return 0;
+		}
+		else
+		{
+			$sql_obj->trans_commit();
+
+			log_write("notification", "cdr_customers_services_ddi", "DDI has been successfully deleted.");
+
+			return 1;
+		}
+	}
+
+
+} // end of class: customer_services_ddi
+
+
+
+
 ?>
