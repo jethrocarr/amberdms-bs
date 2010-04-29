8cec8cff65c8886c27fd1f5c1c06cca31a4b5205
Jethro Carr <jethro.carr@jethrocarr.com>
2010-04-29 12:28:32 +0000

    * Implemented UI logic for managing CDR and Netflow usage record source databases

--- admin/config_services-process.php
+++ admin/config_services-process.php
@@ -22,7 +22,84 @@ if (user_permissions_get("admin"))
 	$data["SERVICE_PARTPERIOD_MODE"]			= @security_form_input_predefined("any", "SERVICE_PARTPERIOD_MODE", 1, "");
 	$data["SERVICE_MIGRATION_MODE"]				= @security_form_input_predefined("checkbox", "SERVICE_MIGRATION_MODE", 0, "");
 
+	if ($GLOBALS["config"]["dangerous_conf_options"] == "enabled")
+	{
+		$data["SERVICE_TRAFFIC_MODE"]			= @security_form_input_predefined("any", "SERVICE_TRAFFIC_MODE", 1, "");
+		$data["SERVICE_CDR_MODE"]			= @security_form_input_predefined("any", "SERVICE_CDR_MODE", 1, "");
 
+		if ($data["SERVICE_TRAFFIC_MODE"] == "external")
+		{
+			$data["SERVICE_TRAFFIC_DB_TYPE"]	= @security_form_input_predefined("any", "SERVICE_TRAFFIC_DB_TYPE", 1, "");
+			$data["SERVICE_TRAFFIC_DB_HOST"]	= @security_form_input_predefined("any", "SERVICE_TRAFFIC_DB_HOST", 0, "");
+			$data["SERVICE_TRAFFIC_DB_NAME"]	= @security_form_input_predefined("any", "SERVICE_TRAFFIC_DB_NAME", 1, "");
+			$data["SERVICE_TRAFFIC_DB_USERNAME"]	= @security_form_input_predefined("any", "SERVICE_TRAFFIC_DB_USERNAME", 1, "");
+			$data["SERVICE_TRAFFIC_DB_PASSWORD"]	= @security_form_input_predefined("any", "SERVICE_TRAFFIC_DB_PASSWORD", 0, "");
+		}
+
+		if ($data["SERVICE_CDR_MODE"] == "external")
+		{
+			$data["SERVICE_CDR_DB_TYPE"]		= @security_form_input_predefined("any", "SERVICE_CDR_DB_TYPE", 1, "");
+			$data["SERVICE_CDR_DB_HOST"]		= @security_form_input_predefined("any", "SERVICE_CDR_DB_HOST", 0, "");
+			$data["SERVICE_CDR_DB_NAME"]		= @security_form_input_predefined("any", "SERVICE_CDR_DB_NAME", 1, "");
+			$data["SERVICE_CDR_DB_USERNAME"]	= @security_form_input_predefined("any", "SERVICE_CDR_DB_USERNAME", 1, "");
+			$data["SERVICE_CDR_DB_PASSWORD"]	= @security_form_input_predefined("any", "SERVICE_CDR_DB_PASSWORD", 0, "");
+		}
+	}
+
+
+
+	/*
+		Test Traffic Database
+	*/
+
+	if ($data["SERVICE_TRAFFIC_DB_TYPE"] == "mysql_netflow_daily")
+	{
+		$obj_sql = New sql_query;
+
+		if (!$obj_sql->session_init("mysql", $data["SERVICE_TRAFFIC_DB_HOST"], $data["SERVICE_TRAFFIC_DB_NAME"], $data["SERVICE_TRAFFIC_DB_USERNAME"], $data["SERVICE_TRAFFIC_DB_PASSWORD"]))
+		{
+			log_write("error", "sql_query", "Unable to connect to traffic service usage database!");
+
+			error_flag_field("SERVICE_TRAFFIC_DB_HOST");
+			error_flag_field("SERVICE_TRAFFIC_DB_NAME");
+			error_flag_field("SERVICE_TRAFFIC_DB_USERNAME");
+			error_flag_field("SERVICE_TRAFFIC_DB_PASSWORD");
+		}
+		else
+		{
+			log_write("notification", "sql_query", "Tested successful connection to traffic usage database");
+
+			$obj_sql->session_terminate();
+		}
+
+	}
+
+
+
+	/*
+		Test CDR Database
+	*/
+
+	if ($data["SERVICE_CDR_DB_TYPE"] == "mysql_cdr_daily")
+	{
+		$obj_sql = New sql_query;
+
+		if (!$obj_sql->session_init("mysql", $data["SERVICE_CDR_DB_HOST"], $data["SERVICE_CDR_DB_NAME"], $data["SERVICE_CDR_DB_USERNAME"], $data["SERVICE_CDR_DB_PASSWORD"]))
+		{
+			log_write("error", "sql_query", "Unable to connect to CDR service usage database!");
+
+			error_flag_field("SERVICE_CDR_DB_HOST");
+			error_flag_field("SERVICE_CDR_DB_NAME");
+			error_flag_field("SERVICE_CDR_DB_USERNAME");
+			error_flag_field("SERVICE_CDR_DB_PASSWORD");
+		}
+		else
+		{
+			log_write("notification", "sql_query", "Tested successful connection to CDR usage database");
+
+			$obj_sql->session_terminate();
+		}
+	}
 
 
 	/*
--- admin/config_services.php
+++ admin/config_services.php
@@ -38,6 +38,202 @@ class page_output
 		$this->obj_form->method = "post";
 
 
+		/*
+			usage services data source configuration
+		
+			We limit this to enabled servers, since it could be misused to try breaking into SQL databases
+			if run by untrusted administrators.
+		*/		
+		if ($GLOBALS["config"]["dangerous_conf_options"] == "enabled")
+		{
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_TRAFFIC_MODE";
+			$structure["type"]					= "radio";
+			$structure["values"]					= array("internal", "external");
+			$structure["translations"]["internal"]			= "Use an internal database for usage records, uploaded via the SOAP API";
+			$structure["translations"]["external"]			= "Use an external SQL database for fetching usage records (such as a netflow DB)";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_TRAFFIC_DB_TYPE";
+			$structure["type"]					= "radio";
+			$structure["values"]					= array("mysql_netflow_daily");
+			$structure["translations"]["mysql_netflow_daily"]	= "MySQL Netflow Daily Tables (traffic_YYYYMMDD)";
+			$structure["options"]["autoselect"]			= "yes";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_TRAFFIC_DB_HOST";
+			$structure["type"]					= "input";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_TRAFFIC_DB_NAME";
+			$structure["type"]					= "input";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_TRAFFIC_DB_USERNAME";
+			$structure["type"]					= "input";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_TRAFFIC_DB_PASSWORD";
+			$structure["type"]					= "input";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+
+			// set javascript actions
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "default", "SERVICE_TRAFFIC_DB_TYPE", "hide");
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "default", "SERVICE_TRAFFIC_DB_HOST", "hide");
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "default", "SERVICE_TRAFFIC_DB_NAME", "hide");
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "default", "SERVICE_TRAFFIC_DB_USERNAME", "hide");
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "default", "SERVICE_TRAFFIC_DB_PASSWORD", "hide");
+
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "external", "SERVICE_TRAFFIC_DB_TYPE", "show");
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "external", "SERVICE_TRAFFIC_DB_HOST", "show");
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "external", "SERVICE_TRAFFIC_DB_NAME", "show");
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "external", "SERVICE_TRAFFIC_DB_USERNAME", "show");
+			$this->obj_form->add_action("SERVICE_TRAFFIC_MODE", "external", "SERVICE_TRAFFIC_DB_PASSWORD", "show");
+		
+
+			// add subform
+			$this->obj_form->subforms["config_usage_traffic"]	= array("SERVICE_TRAFFIC_MODE", "SERVICE_TRAFFIC_DB_TYPE", "SERVICE_TRAFFIC_DB_HOST", "SERVICE_TRAFFIC_DB_NAME", "SERVICE_TRAFFIC_DB_USERNAME", "SERVICE_TRAFFIC_DB_PASSWORD");
+		}
+		else
+		{
+			//
+			// explain that the configuration is locked and tell the user the current source of records.
+			//
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_TRAFFIC_MSG";
+			$structure["type"]					= "message";
+
+			if (sql_get_singlevalue("SELECT value FROM config WHERE name='SERVICE_TRAFFIC_MODE' LIMIT 1") == "internal")
+			{
+				$structure["defaultvalue"]			= "<p>Using internal database for usage records (this configuration is locked by the system administrator)</p>";
+			}
+			else
+			{
+				$structure["defaultvalue"]			= "<p>Use external database for usage records (this configuration is locked by the system administrator)</p>";
+			}
+
+			$structure["options"]["css_row_class"]			= "table_highlight_info";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+
+			$this->obj_form->subforms["config_usage_traffic"]	= array("SERVICE_TRAFFIC_MSG");
+		}
+
+
+
+		/*
+			cdr services data source configuration
+		
+			We limit this to enabled servers, since it could be misused to try breaking into SQL databases
+			if run by untrusted administrators.
+		*/		
+		if ($GLOBALS["config"]["dangerous_conf_options"] == "enabled")
+		{
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_CDR_MODE";
+			$structure["type"]					= "radio";
+			$structure["values"]					= array("internal", "external");
+			$structure["translations"]["internal"]			= "Use an internal database for usage records, uploaded via the SOAP API";
+			$structure["translations"]["external"]			= "Use an external SQL database for fetching usage records.";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_CDR_DB_TYPE";
+			$structure["type"]					= "radio";
+			$structure["values"]					= array("mysql_asterisk");
+			$structure["translations"]["mysql_asterisk"]		= "MySQL-based Asterisk CDR Database";
+			$structure["options"]["autoselect"]			= "yes";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_CDR_DB_HOST";
+			$structure["type"]					= "input";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_CDR_DB_NAME";
+			$structure["type"]					= "input";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_CDR_DB_USERNAME";
+			$structure["type"]					= "input";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_CDR_DB_PASSWORD";
+			$structure["type"]					= "input";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+
+			// set javascript actions
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "default", "SERVICE_CDR_DB_TYPE", "hide");
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "default", "SERVICE_CDR_DB_HOST", "hide");
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "default", "SERVICE_CDR_DB_NAME", "hide");
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "default", "SERVICE_CDR_DB_USERNAME", "hide");
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "default", "SERVICE_CDR_DB_PASSWORD", "hide");
+
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "external", "SERVICE_CDR_DB_TYPE", "show");
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "external", "SERVICE_CDR_DB_HOST", "show");
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "external", "SERVICE_CDR_DB_NAME", "show");
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "external", "SERVICE_CDR_DB_USERNAME", "show");
+			$this->obj_form->add_action("SERVICE_CDR_MODE", "external", "SERVICE_CDR_DB_PASSWORD", "show");
+		
+
+			// add subform
+			$this->obj_form->subforms["config_usage_cdr"]	= array("SERVICE_CDR_MODE", "SERVICE_CDR_DB_TYPE", "SERVICE_CDR_DB_HOST", "SERVICE_CDR_DB_NAME", "SERVICE_CDR_DB_USERNAME", "SERVICE_CDR_DB_PASSWORD");
+		}
+		else
+		{
+			//
+			// explain that the configuration is locked and tell the user the current source of records.
+			//
+			$structure = NULL;
+			$structure["fieldname"]					= "SERVICE_CDR_MSG";
+			$structure["type"]					= "message";
+
+			if (sql_get_singlevalue("SELECT value FROM config WHERE name='SERVICE_CDR_MODE' LIMIT 1") == "internal")
+			{
+				$structure["defaultvalue"]			= "<p>Using internal database for usage records (this configuration is locked by the system administrator)</p>";
+			}
+			else
+			{
+				$structure["defaultvalue"]			= "<p>Use external database for usage records (this configuration is locked by the system administrator)</p>";
+			}
+
+			$structure["options"]["css_row_class"]			= "table_highlight_info";
+			$structure["options"]["no_translate_fieldname"]		= "yes";
+			$this->obj_form->add_input($structure);
+
+
+			$this->obj_form->subforms["config_usage_cdr"]		= array("SERVICE_CDR_MSG");
+		}
+
+
+
+
+
+
+
 		// migration mode options
 		$structure = NULL;
 		$structure["fieldname"]				= "SERVICE_MIGRATION_MODE";
