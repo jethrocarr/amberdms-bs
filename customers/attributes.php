5ffef61100311c2eb0d91b84e4d41b5c402c1e2a
Thomas Howard <thomas.howard@amberdms.com>
2010-07-01 06:12:40 +0000

    Initial work on Issue 132: Customer Attributes Page

---
+++ customers/attributes.php
@@ -0,0 +1,127 @@
+<?php
+/*
+	customers/journal.php
+	
+	access: customers_view (read-only)
+		customers_write (write access)
+
+	Standard journal for customer records and audit trail.
+*/
+
+
+
+class page_output
+{
+	var $id;
+	var $obj_menu_nav;
+	var $obj_journal;
+
+
+	function page_output()
+	{
+		// fetch variables
+		$this->id = @security_script_input('/^[0-9]*$/', $_GET["id"]);
+
+		// define the navigiation menu
+		$this->obj_menu_nav = New menu_nav;
+
+		$this->obj_menu_nav->add_item("Customer's Details", "page=customers/view.php&id=". $this->id ."");
+
+		if (sql_get_singlevalue("SELECT value FROM config WHERE name='MODULE_CUSTOMER_PORTAL' LIMIT 1") == "enabled")
+		{
+			$this->obj_menu_nav->add_item("Portal Options", "page=customers/portal.php&id=". $this->id ."");
+		}
+
+		$this->obj_menu_nav->add_item("Customer's Journal", "page=customers/journal.php&id=". $this->id ."");
+		$this->obj_menu_nav->add_item("Customer's Attributes", "page=customers/attributes.php&id=". $this->id ."", TRUE);
+		$this->obj_menu_nav->add_item("Customer's Invoices", "page=customers/invoices.php&id=". $this->id ."");
+		$this->obj_menu_nav->add_item("Customer's Services", "page=customers/services.php&id=". $this->id ."");
+
+		if (user_permissions_get("customers_write"))
+		{
+			$this->obj_menu_nav->add_item("Delete Customer", "page=customers/delete.php&id=". $this->id ."");
+		}
+	}
+
+
+
+	function check_permissions()
+	{
+		return user_permissions_get("customers_view");
+	}
+
+
+
+	function check_requirements()
+	{
+		// verify that customer exists
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "SELECT id FROM customers WHERE id='". $this->id ."'";
+		$sql_obj->execute();
+
+		if (!$sql_obj->num_rows())
+		{
+			log_write("error", "page_output", "The requested customer (". $this->id .") does not exist - possibly the customer has been deleted.");
+			return 0;
+		}
+
+		unset($sql_obj);
+
+
+		return 1;
+	}
+
+
+	function execute()
+	{
+		/*
+			Define the journal structure
+		*/
+
+		// basic
+		//$this->obj_journal		= New journal_display;
+		//$this->obj_journal->journalname	= "customers";
+		
+		// set the pages to use for forms or file downloads
+		//$this->obj_journal->prepare_set_form_process_page("customers/journal-edit.php");
+		//$this->obj_journal->prepare_set_download_page("customers/journal-download-process.php");
+		
+		// configure options form
+		//$this->obj_journal->prepare_predefined_optionform();
+		//$this->obj_journal->add_fixed_option("id", $this->id);
+
+		// load options form
+		//$this->obj_journal->load_options_form();
+
+		// define SQL structure
+		//$this->obj_journal->sql_obj->prepare_sql_addwhere("customid='". $this->id ."'");		// we only want journal entries for this ticket!
+
+		// process SQL			
+		//$this->obj_journal->generate_sql();
+		//$this->obj_journal->load_data();
+	}
+
+
+	function render_html()
+	{
+		// display header
+		print "<h3>CUSTOMER ATTRIBUTES</h3><br>";
+		print "<p>You can add customer attributes here</p>";
+
+		if (user_permissions_get("customers_write"))
+		{
+			print "<div class='add_attribute_box'>";
+			
+			print "</div>";
+			//print "<p><b><a class=\"button\" href=\"index.php?page=customers/journal-edit.php&type=text&id=". $this->id ."\">Add new journal entry</a> <a class=\"button\" href=\"index.php?page=customers/journal-edit.php&type=file&id=". $this->id ."\">Upload File</a></b></p>";
+		}
+
+//		// display options form
+//		$this->obj_journal->render_options_form();
+//		
+//		// display journal
+//		$this->obj_journal->render_journal();
+	}
+
+} // end of page_output class
+?>
---
+++ include/attributes/inc_attributes.php
@@ -0,0 +1,619 @@
+<?php
+/*
+	attributes/inc_attributes.php
+
+	Provides classes for managing attributes.
+*/
+
+
+
+
+/*
+	CLASS: attributes
+
+	Provides functions for managing attributes
+*/
+
+
+class attributes_keys {
+
+	var $id;		// holds attribute key ID
+	var $data;		// holds values of record fields
+
+	/*
+		verify_id
+
+		Checks that the provided ID is a valid attribute key
+
+		Results
+		0	Failure to find the ID
+		1	Success - attribute key exists
+	*/
+
+	function verify_id()
+	{
+		log_debug("inc_attributes", "Executing verify_id()");
+
+		if ($this->id)
+		{
+			$sql_obj		= New sql_query;
+			$sql_obj->string	= "SELECT id FROM `attributes_keys` WHERE id='". $this->id ."' LIMIT 1";
+			$sql_obj->execute();
+
+			if ($sql_obj->num_rows())
+			{
+				return 1;
+			}
+		}
+
+		return 0;
+
+	} // end of verify_id
+
+	/*
+		verify_id
+
+		Checks that the provided ID is a valid attribute value
+
+		Results
+		0	Failure to find the ID
+		1	Success - attribute value exists
+	*/
+
+	function verify_id()
+	{
+		log_debug("inc_attributes", "Executing verify_id()");
+
+		if ($this->id)
+		{
+			$sql_obj		= New sql_query;
+			$sql_obj->string	= "SELECT id FROM `attributes_values` WHERE id='". $this->id ."' LIMIT 1";
+			$sql_obj->execute();
+
+			if ($sql_obj->num_rows())
+			{
+				return 1;
+			}
+		}
+
+		return 0;
+
+	} // end of verify_id
+	
+
+	/*
+		check_delete_lock
+
+		Returns whether the product can be safely deleted or not.
+
+		Results
+		0	Unlocked
+		1	Locked
+	*/
+
+	function check_delete_lock()
+	{
+		log_debug("inc_attributes", "Executing check_delete_lock()");
+
+		// check if the product belongs to any invoices
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "SELECT id FROM account_items WHERE (type='product' OR type='time') AND customid='". $this->id ."'";
+		$sql_obj->execute();
+
+		if ($sql_obj->num_rows())
+		{
+			return 1;
+		}
+	
+		unset($sql_obj);
+
+
+
+		// unlocked
+		return 0;
+
+	}  // end of check_delete_lock
+
+
+
+	/*
+		load_data
+
+		Load the product's information into the $this->data array.
+
+		Returns
+		0	failure
+		1	success
+	*/
+	function load_data()
+	{
+		log_debug("inc_attributes", "Executing load_data()");
+
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "SELECT * FROM attributes_values WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+		if ($sql_obj->num_rows())
+		{
+			$sql_obj->fetch_array();
+
+			$this->data = $sql_obj->data[0];	
+
+			return 1;
+		}
+
+		// failure
+		return 0;
+
+	} // end of load_data
+
+
+
+
+	/*
+		action_create
+
+		Create a new product based on the data in $this->data
+
+		Results
+		0	Failure
+		#	Success - return ID
+	*/
+	function action_create()
+	{
+		log_debug("inc_attributes", "Executing action_create()");
+
+		// create a new product
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "INSERT INTO `attributes_keys` ( key_name ) VALUES ( '".$this->data["key_name"]."' )";
+		
+		
+
+		
+		$sql_obj->execute();
+
+		$this->id = $sql_obj->fetch_insert_id();
+
+
+		return $this->id;
+
+	} // end of action_create
+
+
+
+	/*
+		action_update_value
+
+		Update an attribute value's details based on the data in $this->data. If no ID is provided,
+		it will first call the action_create function.
+
+		Returns
+		0	failure 
+		#	success - returns the ID
+	*/
+	function action_update()
+	{
+		log_debug("inc_attributes", "Executing action_update()");
+
+		/*
+			Start Transaction
+		*/
+		$sql_obj = New sql_query;
+		$sql_obj->trans_begin();
+
+
+		/*
+			If no ID exists, create a new attribute value first
+
+			(Note: if this function has been called by the action_update() wrapper function
+			this step will already have been performed and we can just ignore it)
+		*/
+		if (!$this->id)
+		{
+			$mode = "create";
+
+			if (!$this->action_create())
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			$mode = "update";
+		}
+
+
+		/*
+			Update product details
+		*/
+
+		$sql_obj->string	= "UPDATE `attributes_keys` SET "
+						."key_name ='".$this->data["key_name"]."'"
+						."WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+
+
+		/*
+			Update Journal
+		*/
+
+		if ($mode == "update")
+		{
+			journal_quickadd_event("attributes_keys", $this->id, "Attribute key updated.");
+		}
+		else
+		{
+			journal_quickadd_event("attributes_keys", $this->id, "Attribute key created.");
+		}
+
+
+	
+		/*
+			Commit
+		*/
+		if (error_check())
+		{
+			$sql_obj->trans_rollback();
+
+			log_write("error", "process", "An error occured whilst attempting to update the attribute key. No changes were made.");
+
+			return 0;
+		}
+		else
+		{
+			$sql_obj->trans_commit();
+
+			if ($mode == "update")
+			{
+				log_write("notification", "inc_attributes", "Attribute key successfully updated.");
+			}
+			else
+			{
+				log_write("notification", "inc_attributes", "Attribute key successfully created.");
+			}
+
+
+			return $this->id;
+		}
+
+	} // end of action_update_details
+	
+
+	/*
+		action_delete_key
+
+		Deletes an attribute key.
+
+		Note: the check_delete_lock function should be executed before calling
+		this function to ensure database integrity.
+
+		Results
+		0	failure
+		1	success
+	*/
+	
+	function action_delete()
+	{
+		log_debug("inc_attributes", "Executing action_delete()");
+
+		// start transaction
+		$sql_obj = New sql_query;
+		$sql_obj->trans_begin();
+
+
+		// delete the product
+		$sql_obj->string	= "DELETE FROM attributes_keys WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+		journal_delete_entire("attributes_keys", $this->id);
+		
+		// commit
+		if (error_check())
+		{
+			$sql_obj->trans_rollback();
+
+			log_write("error". "inc_attributes". "An error occured whilst attempting to delete the product. No changes have been made.");
+		}
+		else
+		{
+			$sql_obj->trans_commit();
+
+			log_write("notification", "inc_attributes", "Attribute Key has been successfully deleted.");
+		}
+
+		return 1;
+	}
+}
+
+
+
+class attributes_values {
+
+	var $id;		// holds attribute ID
+	var $data;		// holds values of record fields
+
+	/*
+		verify_id
+
+		Checks that the provided ID is a valid attribute key
+
+		Results
+		0	Failure to find the ID
+		1	Success - attribute key exists
+	*/
+
+	function verify_id()
+	{
+		log_debug("inc_attributes", "Executing verify_id()");
+
+		if ($this->id)
+		{
+			$sql_obj		= New sql_query;
+			$sql_obj->string	= "SELECT id FROM `attributes_values` WHERE id='". $this->id ."' LIMIT 1";
+			$sql_obj->execute();
+
+			if ($sql_obj->num_rows())
+			{
+				return 1;
+			}
+		}
+
+		return 0;
+
+	} // end of verify_id
+	
+
+	/*
+		check_delete_lock
+
+		Returns whether the product can be safely deleted or not.
+
+		Results
+		0	Unlocked
+		1	Locked
+	*/
+
+	function check_delete_lock()
+	{
+		log_debug("inc_attributes", "Executing check_delete_lock()");
+
+		// check if the product belongs to any invoices
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "SELECT id FROM account_items WHERE (type='product' OR type='time') AND customid='". $this->id ."'";
+		$sql_obj->execute();
+
+		if ($sql_obj->num_rows())
+		{
+			return 1;
+		}
+	
+		unset($sql_obj);
+
+
+
+		// unlocked
+		return 0;
+
+	}  // end of check_delete_lock
+
+
+
+	/*
+		load_data
+
+		Load the product's information into the $this->data array.
+
+		Returns
+		0	failure
+		1	success
+	*/
+	function load_data()
+	{
+		log_debug("inc_attributes", "Executing load_data()");
+
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "SELECT * FROM attributes_values WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+		if ($sql_obj->num_rows())
+		{
+			$sql_obj->fetch_array();
+
+			$this->data = $sql_obj->data[0];	
+
+			return 1;
+		}
+
+		// failure
+		return 0;
+
+	} // end of load_data
+
+
+
+
+	/*
+		action_create
+
+		Create a new product based on the data in $this->data
+
+		Results
+		0	Failure
+		#	Success - return ID
+	*/
+	function action_create()
+	{
+		log_debug("inc_attributes", "Executing action_create()");
+
+		// create a new product
+		$sql_obj		= New sql_query;
+		$sql_obj->string	= "INSERT INTO `attributes_values` ( id_owner ) VALUES ( '".$this->data["id_owner"]."' )";
+		
+		$sql_obj->execute();
+
+		$this->id = $sql_obj->fetch_insert_id();
+
+
+		return $this->id;
+
+	} // end of action_create
+
+
+
+	/*
+		action_update_value
+
+		Update an attribute value's details based on the data in $this->data. If no ID is provided,
+		it will first call the action_create function.
+
+		Returns
+		0	failure 
+		#	success - returns the ID
+	*/
+	function action_update()
+	{
+		log_debug("inc_attributes", "Executing action_update()");
+
+		/*
+			Start Transaction
+		*/
+		$sql_obj = New sql_query;
+		$sql_obj->trans_begin();
+
+
+		/*
+			If no ID exists, create a new attribute value first
+
+			(Note: if this function has been called by the action_update() wrapper function
+			this step will already have been performed and we can just ignore it)
+		*/
+		if (!$this->id)
+		{
+			$mode = "create";
+
+			if (!$this->action_create())
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			$mode = "update";
+		}
+
+
+		/*
+			Update product details
+		*/
+
+		$sql_obj->string	= "UPDATE `attributes_values` SET "
+						."id_owner='".$this->data["id_owner"]."',"
+						."id_key'".$this->data["id_key"]."',"
+						."type'".$this->data["type"]."',"
+						."value'".$this->data["value"]."'"
+						."WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+
+
+		/*
+			Update Journal
+		*/
+
+		if ($mode == "update")
+		{
+			journal_quickadd_event("attributes_values", $this->id, "Attribute value updated.");
+		}
+		else
+		{
+			journal_quickadd_event("attributes_values", $this->id, "Attribute value created.");
+		}
+
+
+	
+		/*
+			Commit
+		*/
+		if (error_check())
+		{
+			$sql_obj->trans_rollback();
+
+			log_write("error", "process", "An error occured whilst attempting to update the attribute value. No changes were made.");
+
+			return 0;
+		}
+		else
+		{
+			$sql_obj->trans_commit();
+
+			if ($mode == "update")
+			{
+				log_write("notification", "inc_attributes", "Attribute value successfully updated.");
+			}
+			else
+			{
+				log_write("notification", "inc_attributes", "Attribute value successfully created.");
+			}
+
+
+			return $this->id;
+		}
+
+	} // end of action_update_details
+
+
+	/*
+		action_delete_value
+
+		Deletes an attribute value.
+
+		Note: the check_delete_lock function should be executed before calling
+		this function to ensure database integrity.
+
+		Results
+		0	failure
+		1	success
+	*/
+	
+	function action_delete()
+	{
+		log_debug("inc_attributes", "Executing action_delete()");
+
+		// start transaction
+		$sql_obj = New sql_query;
+		$sql_obj->trans_begin();
+
+
+		// delete the product
+		$sql_obj->string	= "DELETE FROM attributes_values WHERE id='". $this->id ."' LIMIT 1";
+		$sql_obj->execute();
+
+
+		journal_delete_entire("attributes_values", $this->id);
+
+		// commit
+		if (error_check())
+		{
+			$sql_obj->trans_rollback();
+
+			log_write("error". "inc_attributes". "An error occured whilst attempting to delete the product. No changes have been made.");
+		}
+		else
+		{
+			$sql_obj->trans_commit();
+
+			log_write("notification", "inc_attributes", "Attribute Key has been successfully deleted.");
+		}
+
+		return 1;
+	}
+
+
+
+
+
+}
+
+
+?>
