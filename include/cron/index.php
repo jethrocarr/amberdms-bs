ae49eb9bdae950152af80e52f2db2616657cd7da
Jethro Carr <jethro.carr@jethrocarr.com>
2010-08-03 13:48:18 +0000

    * Implemented bug fix for SQL queries on cron scripts when executing on a hosted platform - issue was caused by a change in approach of Amberphplib's handling of DB sessions. (issue 177)

--- include/cron/index.php
+++ include/cron/index.php
@@ -95,6 +95,18 @@ if ($GLOBALS["config"]["instance"] == "hosted")
 		}
 
 
+		/*
+			Bootstrap Framework
+	
+			We couldn't use the Amberphplib framework to connect to the database, however
+			now that we have connected, we can force set the default values and it will
+			use the connection we have established as the default for all queries.
+		*/
+
+		$GLOBALS["cache"]["database_default_link"]	= $link;
+		$GLOBALS["cache"]["database_default_type"]	= "mysql";
+
+
 		// execute the page functions
 		page_execute();
 	}
