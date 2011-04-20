3ea593a5c040d96be1830ee16f28cd445dc06cdc
Jethro Carr <jethro.carr@jethrocarr.com>
2011-04-20 04:55:21 +0000

    * Updated menu links for customer orders page. (issue 90)

--- customers/journal-edit.php
+++ customers/journal-edit.php
@@ -40,6 +40,7 @@ class page_output
 
 		$this->obj_menu_nav->add_item("Customer's Journal", "page=customers/journal.php&id=". $this->id ."", TRUE);
 		$this->obj_menu_nav->add_item("Customer's Attributes", "page=customers/attributes.php&id_customer=". $this->id ."");
+		$this->obj_menu_nav->add_item("Customer's Orders", "page=customers/orders.php&id_customer=". $this->id ."");
 		$this->obj_menu_nav->add_item("Customer's Invoices", "page=customers/invoices.php&id=". $this->id ."");
 		$this->obj_menu_nav->add_item("Customer's Services", "page=customers/services.php&id=". $this->id ."");
 		$this->obj_menu_nav->add_item("Delete Customer", "page=customers/delete.php&id=". $this->id ."");
