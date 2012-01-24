3d0e0c785914b071e042804cbdb7fbd1c7e96b00
Nathan Humpheries <nathan@prophecy.net.nz>
2012-01-24 22:32:33 +0000

    Added new menu item for 'Resellers Customers' in the customers section (issue 335)

--- customers/services.php
+++ customers/services.php
@@ -48,6 +48,7 @@ class page_output
 		$this->obj_menu_nav->add_item("Customer's Invoices", "page=customers/invoices.php&id=". $this->id ."");
 		$this->obj_menu_nav->add_item("Customer's Credit", "page=customers/credit.php&id_customer=". $this->id ."");
 		$this->obj_menu_nav->add_item("Customer's Services", "page=customers/services.php&id=". $this->id ."", TRUE);
+		$this->obj_menu_nav->add_item("Resellers Customers", "page=customers/reseller.php&id=". $this->id ."");
 
 		if (user_permissions_get("customers_write"))
 		{
