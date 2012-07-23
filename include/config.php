42fa0de23aea057afa46aa155c400639d30727db
Nathan Humpheries <nathan@prophecy.net.nz>
2012-07-23 03:13:15 +0000

    * Fixed version number. :-)

--- include/config.php
+++ include/config.php
@@ -23,7 +23,7 @@ $GLOBALS["config"] = array();
 
 // define the application details
 $GLOBALS["config"]["app_name"]			= "Amberdms Billing System";
-$GLOBALS["config"]["app_version"]		= "1.5.0_beta_9";
+$GLOBALS["config"]["app_version"]		= "1.5.0_beta_10";
 
 // define the schema version required
 $GLOBALS["config"]["schema_version"]		= "20120723";
