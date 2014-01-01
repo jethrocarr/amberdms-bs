1c30cbbf99e6dbfa210cfc772a6f911b6f15b742
Jethro Carr <jethro.carr@jethrocarr.com>
2014-01-01 17:42:31 +1300

    Schema updater for ABS now will update both single and multi/hosted instance installations.

--- help/resources/schema_update.pl
+++ help/resources/schema_update.pl
@@ -3,10 +3,11 @@
 # help/resources/schema_update.pl
 #
 # This script checks the current version of the database for the Amberdms Billing
-# System and applys any upgrades.
+# System and applies any upgrades.
 #
 # It reads in the MySQL authentication data from the configuration file to provide
-# interaction-less upgrades.
+# interaction-less upgrades. It supports upgrading both single-instance
+# installations and multi-instance/"hosted" installations.
 # 
 #
 
@@ -35,7 +36,7 @@ GetOptions ("cfgfile=s"		=> \$opt_cfgfile,	"c=s"	=> \$opt_cfgfile,
 
 if ($help)
 {
-	print "AOConf Schema Management Utility\n";
+	print "Amberdms Billing System Schema Management Utility\n";
 	print "\n";
 	print "Usage: schema_manage.pl --schemadir=<schemadir>\n";
 	print "\n";
@@ -56,24 +57,29 @@ if ($help)
 # check options
 if (!$opt_cfgfile)
 {
-	die("Error: No config file specified.\n");
+	die("Error: No config file specified. Run with --help\n");
 }
 
 if (!$opt_schema)
 {
-	die("Error: No schmea directory provided.\n");
+	die("Error: No schema directory provided. Run with --help\n");
 }
 
 
 ## GET CONFIG ##
-my ($db_host, $db_name, $db_user, $db_pass);
+my ($db_instance_mode, $db_host, $db_name, $db_user, $db_pass);
 
 open(CFG, $opt_cfgfile) || die("Error: Unable to open config file $opt_cfgfile");
 
 while (my $line = <CFG>)
 {
 	chomp($line);
-	
+		
+	if ($line =~ /^\$config\["instance"\]\s*=\s*"(\S*)";/)
+	{
+		$db_instance_mode = $1;
+	}
+
 	if ($line =~ /^\$config\["db_host"\]\s*=\s*"(\S*)";/)
 	{
 		$db_host = $1;
@@ -99,6 +105,11 @@ close(CFG);
 
 
 # check that DB config was gathered.
+if (!$db_instance_mode)
+{
+	$db_instance_mode = "single"; # assume single instance, unless specifically configured otherwise.
+}
+
 if (!$db_host || !$db_name || !$db_user)
 {
 	die("Error: Unable to gather required information from config file $opt_cfgfile");
@@ -106,81 +117,140 @@ if (!$db_host || !$db_name || !$db_user)
 
 
 
-## PROGRAM ##
+## ASSEMBLE DB ARRAY ##
 
-# connect to MySQL DB.
-my $mysql_handle = DBI->connect("dbi:mysql:database=$db_name;host=$db_host;user=$db_user;password=$db_pass") || die("Error: Unable to connect to MySQL database: $DBI::errstr\n");
-my ($mysql_string, $mysql_result, $mysql_data);
+my @db_instance_list;
 
+# Make an array of all the databases to go and upgrade. If we are a single
+# instance it will only ever be one, but if we're hosted, we need to assemble
+# all the different DBs to upgrade.
 
-
-# 1. Fetch schema version
-print "Fetching current version from the database...\n" if $debug;
-
-$mysql_string = "SELECT value FROM `config` WHERE name='SCHEMA_VERSION'";
-$mysql_result = $mysql_handle->prepare($mysql_string) || die("Error: SQL query ($mysql_string) failed: $DBI::errstr\n");
-$mysql_result->execute;
-
-while ($mysql_data = $mysql_result->fetchrow_hashref())
+if ($db_instance_mode eq "hosted")
 {
-	$opt_version_schema = $mysql_data->{value};
-}
-$mysql_result->finish;
+	# Hosted mode - connect to the hosted instances DB table and use that
+	# to discover all the other DBs to connect to.
+	
+	print "Running in a hosted mode environment. Fetching all instances to upgrade...\n" if $debug;
 
+	# Connect to MySQL DB.
+	my $mysql_handle = DBI->connect("dbi:mysql:database=${db_name}_instances;host=$db_host;user=$db_user;password=$db_pass") || die("Error: Unable to connect to MySQL database: $DBI::errstr\n");
+	my ($mysql_string, $mysql_result, $mysql_data);
 
+	# Fetch instances
+	$mysql_string = "SELECT instanceid FROM `instances`";
+	$mysql_result = $mysql_handle->prepare($mysql_string) || die("Error: SQL query ($mysql_string) failed: $DBI::errstr\n");
+	$mysql_result->execute;
 
-# If there was no version returned, it means the user is running the Amberdms 
-# Billing System version 1.0.0 (pre-upgrade support)
+	while ($mysql_data = $mysql_result->fetchrow_hashref())
+	{
+		push(@db_instance_list, $db_name ."_". $mysql_data->{'instanceid'});
+	}
 
-if (!$opt_version_schema)
-{
-	$opt_version_schema = "20090217";
-}
+	$mysql_result->finish;
 
+	# Determine latest version and update instances DB version to suit
+	my @upgrade_files  = glob("$opt_schema/version_*_upgrade.sql");
+	$upgrade_files[-1] =~ /_([0-9]{4}[0-9]{2}[0-9]{2}[0-9]*)_/;
+	my $latestversion  = $1;
 
-## 2. Upgrade Schema
-
-print "Existing schema version is: $opt_version_schema\n" if $debug;
-
-# get a list of upgrade files
-my @data	= glob("$opt_schema/version_*_upgrade.sql");
-my $count	= scalar @data;
-
-if ($count == 0)
-{
-	print "No data needs to be imported.\n" if $debug;
+	# Update the instances DB version for hosted version
+	# Note: individual instance DBs also need upgrading, which will happen as part of the upgrade.
+	$mysql_string = "UPDATE `config` SET value='$latestversion' WHERE name='SCHEMA_VERSION' LIMIT 1";
+	$mysql_result = $mysql_handle->prepare($mysql_string) || die("Error: SQL query ($mysql_string) failed: $DBI::errstr\n");
+	$mysql_result->execute;
 }
 else
 {
-	print "Applying any upgrade SQL files that match...\n" if $debug;
-	
-	my $latestversion;
-	foreach my $sqlfile (@data)
+	# Single Instance Mode - just the one DB
+	push(@db_instance_list, $db_name);
+}
+
+
+
+## PROGRAM ##
+
+foreach my $db_instance_name (@db_instance_list)
+{
+	print "Instance: $db_instance_name\n" if $debug;
+
+	# connect to MySQL DB.
+	my $mysql_handle = DBI->connect("dbi:mysql:database=$db_instance_name;host=$db_host;user=$db_user;password=$db_pass") || die("Error: Unable to connect to MySQL database: $DBI::errstr\n");
+	my ($mysql_string, $mysql_result, $mysql_data);
+
+	# 1. Fetch schema version
+	print "Fetching current version from the database...\n" if $debug;
+
+	$mysql_string = "SELECT value FROM `config` WHERE name='SCHEMA_VERSION'";
+	$mysql_result = $mysql_handle->prepare($mysql_string) || die("Error: SQL query ($mysql_string) failed: $DBI::errstr\n");
+	$mysql_result->execute;
+
+	while ($mysql_data = $mysql_result->fetchrow_hashref())
 	{
-		if ($sqlfile =~ /_([0-9]{4}[0-9]{2}[0-9]{2}[0-9]*)_/)
+		$opt_version_schema = $mysql_data->{value};
+	}
+	$mysql_result->finish;
+
+
+
+	# If there was no version returned, it means the user is running the Amberdms 
+	# Billing System version 1.0.0 (pre-upgrade support)
+
+	if (!$opt_version_schema)
+	{
+		$opt_version_schema = "20090217";
+	}
+
+
+	## 2. Upgrade Schema
+
+	print "Existing schema version is: $opt_version_schema\n" if $debug;
+
+	# get a list of upgrade files
+	my @data	= glob("$opt_schema/version_*_upgrade.sql");
+	my $count	= scalar @data;
+
+	if ($count == 0)
+	{
+		print "No data needs to be imported.\n" if $debug;
+	}
+	else
+	{
+		print "Applying any upgrade SQL files that match...\n" if $debug;
+		
+		my $latestversion;
+		foreach my $sqlfile (@data)
 		{
-			$latestversion = $1;
-			
-			if ($latestversion > $opt_version_schema)
+			if ($sqlfile =~ /_([0-9]{4}[0-9]{2}[0-9]{2}[0-9]*)_/)
 			{
-				# need to import this schema upgrade file
-				print "Importing file $sqlfile\n" if $debug;
-				import_sql($sqlfile, $mysql_handle);
+				$latestversion = $1;
+				
+				if ($latestversion > $opt_version_schema)
+				{
+					# Need to import this schema upgrade file
+					print "Importing file $sqlfile\n" if $debug;
+					import_sql($sqlfile, $mysql_handle);
+
+					# update the schema version in the DB
+					$mysql_string = "UPDATE `config` SET value='$latestversion' WHERE name='SCHEMA_VERSION' LIMIT 1";
+					$mysql_result = $mysql_handle->prepare($mysql_string) || die("Error: SQL query ($mysql_string) failed: $DBI::errstr\n");
+					$mysql_result->execute;
+				}
+				else
+				{
+					print "Schema version $latestversion has already been applied, skipping...\n" if $debug;
+				}
 			}
 			else
 			{
-				print "Schema version $latestversion has already been applied, skipping...\n" if $debug;
+				print "Warning: Incorrectly named SQL upgrade file: $sqlfile\n";
 			}
 		}
-		else
-		{
-			print "Warning: Incorrectly named SQL upgrade file: $sqlfile\n";
-		}
 	}
+
+	# disconnect from MySQL
+	$mysql_handle->disconnect;
 }
 
-# disconnect from MySQL
-$mysql_handle->disconnect;
 
 print "Schema installation complete!\n" if $debug;
 exit 0;
